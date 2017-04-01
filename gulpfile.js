1cb57f77912b549de4e1c35c326cd74a26d8b65d
omgimanerd <alvin.lin.dev@gmail.com>
2017-04-01 16:02:09 -0400

    Upgraded system to phase out makefile

--- gulpfile.js
+++ gulpfile.js
@@ -8,42 +8,12 @@ var path = require('path');
 var del = require('del');
 var gulp = require('gulp');
 var gulpChanged = require('gulp-changed');
-var gulpPDFLatex = require('./custompdflatex');
+var gulpPdflatex = require('gulp-pdflatex');
+var gulpPdflatex2 = require('../gulp-pdflatex2/gulp-pdflatex2');
 var gulpPlumber = require('gulp-plumber');
 var gulpRename = require('gulp-rename');
-var gulpShell = require('gulp-shell');
-var gulpPrint = require('gulp-print');
-var through = require('through2');
 
 gulp.task('default', ['latex']);
-
-var pdflatex = (options) => through.obj((file, encoding, next) => {
-    if (file.isStream()) {
-        file.extname = '.pdf';
-        file.path = replaceExtension(file.path, file.extname);
-        const output = node_pdflatex_1.default(file.contents, {
-            shellEscape: options.shellEscape,
-            texInputs: [file.base, ...options.texInputs],
-        });
-        file.contents = output;
-        next(null, file);
-    }
-    else if (file.isBuffer()) {
-        file.extname = '.pdf';
-        file.path = replaceExtension(file.path, file.extname);
-        const input = through();
-        input.end(file.contents);
-        const output = node_pdflatex_1.default(input, {
-            shellEscape: options.shellEscape,
-            texInputs: [file.base, ...options.texInputs],
-        });
-        getRawBody(output)
-            .then(data => (file.contents = data) && next(null, file))
-            .catch(err => next(err));
-    }
-    else
-        next(null, file);
-})
 
 var getOutputFile = function(texFile) {
   texFile = path.parse(texFile);
@@ -52,20 +22,14 @@ var getOutputFile = function(texFile) {
 
 gulp.task('latex', function() {
   return gulp.src('./latex/**/*.tex')
-    .pipe(gulpPlumber(function() {
-      console.log(arguments);
-    }))
+    .pipe(gulpPlumber())
     .pipe(gulpChanged('./latex', { transformPath: getOutputFile }))
-    .pipe(gulpPDFLatex({
-      shellEscape: true,
-      texInputs: ['./latex/cls']
+    .pipe(gulpPdflatex2({
+      TEXINPUTS: ['./latex/cls']
     }))
     .pipe(gulpRename((path) => {
       path.dirname += '/output';
       path.extname = '.pdf';
-    }))
-    .pipe(gulpPrint((filename) => {
-      return `Compiled ${filename}`;
     }))
     .pipe(gulp.dest('./latex'))
 });
